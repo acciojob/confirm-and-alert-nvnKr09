1f4ef85505d9d75c8140250e9d06439c54d4acc2
Naveen Kumar <134830093+nvnKr09@users.noreply.github.com>
2023-10-01 22:36:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+const isConfirmed = confirm("Do you want to proceed?");
+
+// If clicked "yes"
+if (isConfirmed) {
+    alert("You clicked OK. Proceeding...");
+// If clicked "cancel"
+} else {
+    alert("You clicked Cancel. Exiting...");
+}
+
